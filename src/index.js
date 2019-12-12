3ba64a6e2a530dd742b9c55a32503307144a9951
toshi-toma <toshitoma1217@gmail.com>
2019-12-12 14:55:15 +0900

    add simple cli

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const [, , firstArg] = process.argv;
+
+if (!firstArg) {
+  console.error("Please pass one argument");
+  process.exit(1);
+}
+
+const msg = `
+  Hello!! ${firstArg} san.
+  I am Toshihisa Tomatsu.
+  GitHub: https://github.com/toshi-toma
+  Twitter: https://twitter.com/toshi__toma
+`;
+
+console.log(msg);
